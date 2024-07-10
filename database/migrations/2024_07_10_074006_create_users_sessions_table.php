1f6451a3cb6f65473be781f35222f30bc3444510
eldar <eldar@iMacPro-eldar.local>
2024-07-10 15:17:19 +0700

    user_auth method: prepare migrations | model | route | controller

--- database/migrations/2024_07_10_074006_create_users_sessions_table.php
+++ database/migrations/2024_07_10_074006_create_users_sessions_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateUsersSessionsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('users_sessions', function (Blueprint $table) {
             $table->id();
-            $table->string('first_name');
-            $table->string('last_name');
-            $table->string('city');
-            $table->string('country');
+            $table->bigInteger('user_id')->unsigned();
+            $table->string('access_token');
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('CASCADE');
         });
     }
 
@@ -29,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('users_sessions');
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Используем метод GET по условию задания (однако для таких запросов необходимо использовать метод POST)
+Route::get('user_auth', 'App\Http\Controllers\UserController@auth');
